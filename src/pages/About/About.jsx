604389ee3f4c5a9edf94dd36f4aaa0b5962523cf
anaissclt <71971929+anaissclt@users.noreply.github.com>
2024-01-02 19:23:22 +0100

    Ajout page à propos

---
+++ src/pages/About/About.jsx
@@ -0,0 +1,15 @@
+// IMPORTS
+import "./about.css";
+
+// ASSETS
+import Banner from "../../assets/about.png";
+
+function About() {
+  return (
+    <section>
+      <img src={Banner} alt="Paysage" />
+    </section>
+  );
+}
+
+export default About;
