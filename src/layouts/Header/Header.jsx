def46ea9825d5da9933a3e74b130209d492ee362
anaissclt <71971929+anaissclt@users.noreply.github.com>
2024-01-02 18:54:34 +0100

    Ajout Header

--- src/layouts/Header/Header.jsx
+++ src/layouts/Header/Header.jsx
@@ -1,9 +1,29 @@
+// IMPORTS
+import { Link, NavLink } from "react-router-dom"; // composants de la bibliothèque React Router
+import "./header.css";
 
-// Layout Header 
+// ASSETS
+import Logo from "../../assets/logo-header.png";
+
+// Layout Header
 function Header() {
-   return (
-     <p>header </p>
-   )
- }
- 
- export default Header
+  return (
+    <nav className="Header_Container">
+      <Link to="/">
+        <img className="Header_Logo" alt="Logo de Kasa" src={Logo} />
+      </Link>
+
+      <div className="Header_Link">
+        <NavLink to="/" className="Link">
+          Accueil
+        </NavLink>
+
+        <NavLink to="/about" className="Link">
+          À Propos
+        </NavLink>
+      </div>
+    </nav>
+  );
+}
+
+export default Header;
