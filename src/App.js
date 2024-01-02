bfdacf53d6b6015025ea5d909bb0cb16b3ec6cdb
anaissclt <71971929+anaissclt@users.noreply.github.com>
2024-01-02 19:08:56 +0100

    Ajout des routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 // IMPORTS
-import {Routes, Route} from 'react-router-dom';
+import { Routes, Route } from "react-router-dom";
 
-import Home from './pages/Home/Home';
+// PAGES
+import Home from "./pages/Home/Home";
+import About from './pages/About/About';
+import Error from "./pages/Error/Error";
 
 // LAYOUTS
-import Header from './layouts/Header/Header';
-import Footer from './layouts/Footer/Footer';
+import Header from "./layouts/Header/Header";
+import Footer from "./layouts/Footer/Footer";
 
-
+// Style
+import './app.css';
 
 // Routes de l'application
 function App() {
-
   return (
     <>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    
-    <Footer />
+      <div className="contenair">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </div>
+      <Footer />
     </>
   );
 }
