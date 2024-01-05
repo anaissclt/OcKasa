0ffcd849a6560e880b8def314fabb2d5b59a3b4b
anaissclt <71971929+anaissclt@users.noreply.github.com>
2024-01-05 17:42:13 +0100

    Ajout du composant Card

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -11,7 +11,7 @@ function Card() {
       <section className='Card_Container'>
          {DATA.map((location) => (  //méthode map : créer une carte pour chaque élément
 
-            <Link to={`/location/${location.id}`} key={location.id} >
+            <Link to={`/logement/${location.id}`} key={location.id} >
                <figure className='Card'>
                   <img className='Card_Image' src={location.cover} alt={location.title}/>        
                   <div className='Card_background'></div>      
