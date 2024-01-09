973bab193dd8a4433cdd70fe2bf694ad65d1d8b5
anaissclt <71971929+anaissclt@users.noreply.github.com>
2024-01-09 19:09:35 +0100

    MAJ footer

--- src/layouts/Footer/Footer.jsx
+++ src/layouts/Footer/Footer.jsx
@@ -1,21 +1,20 @@
 // IMPORTS
-import { Link } from 'react-router-dom'
-import './footer.css'
+import { Link } from "react-router-dom";
+import "./footer.css";
 
 // ASSETS
-import LogoFooter from '../../assets/logo-footer.png'
-
+import LogoFooter from "../../assets/logo-footer.png";
 
 // Layout Footer
-function Footer() {
-   return (
-      <footer className='Footer_Container'>
-         <Link to="/">
-            <img src={LogoFooter} alt='Logo de Kasa'/>
-         </Link>
-         <p className='Footer_Text'>© 2020 Kasa. All rights reserved</p>
-      </footer>
-   )
- }
- 
- export default Footer
+const Footer = () => {
+  return (
+    <footer className="Footer_Container">
+      <Link to="/">
+        <img src={LogoFooter} alt="Logo de Kasa" />
+      </Link>
+      <p className="Footer_Text">© 2020 Kasa. All rights reserved</p>
+    </footer>
+  );
+};
+
+export default Footer;
