fe6922b1ea3105455f247c90bfb0383165350ce6
anaissclt <71971929+anaissclt@users.noreply.github.com>
2024-01-10 17:53:20 +0100

    Ajout du carrousel

--- src/components/Slideshow/Slideshow.jsx
+++ src/components/Slideshow/Slideshow.jsx
@@ -1,15 +1,49 @@
-// Import
-import React from 'react';
+// IMPORTS
+import React, { useState } from "react";
+import "./slideshow.css";
+
+// ASSETS
+import arrowright from "../../assets/arrow-right.png";
+import arrowleft from "../../assets/arrow-left.png";
 
 const Slideshow = ({ pictures }) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  
+  const NextSlide = () => {
+    setCurrentIndex((Next) => (Next === pictures.length - 1 ? 0 : Next + 1));
+  };
+
+  const PrevSlide = () => {
+    setCurrentIndex((Previous) =>
+      Previous === 0 ? pictures.length - 1 : Previous - 1
+    );
+  };
+
   return (
-    <div className="Slideshow">
-      {pictures.map((picture, index) => (
-        <div key={index} className={`slide ${index === 0 ? 'active' : ''}`}>
-          <img src={picture} alt={`Slide ${index + 1}`} />
-        </div>
-      ))}
-    </div>
+    <section className="Slide_Container">
+        {/* Affiche les éléments seulement si plusieurs images existent */}
+      {pictures.length > 1 && (
+        <>
+          <p className="Slide_Number">
+            {currentIndex + 1} / {pictures.length}
+          </p>
+          <img
+            className="Slider_Right"
+            src={arrowright}
+            alt="Flèche droite"
+            onClick={NextSlide}
+          />
+          <img
+            className="Slider_Left"
+            src={arrowleft}
+            alt="Flèche gauche"
+            onClick={PrevSlide}
+          />
+        </>
+      )}
+      <img className="Slide_Img" src={pictures[currentIndex]} alt="Images du logement" />
+    </section>
   );
 };
 
